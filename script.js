a14d633b6986d73e60ad3c4cf7a3a86d161d24a9
Kolesnyk Mykhailo <devhomeworks@gmail.com>
2024-07-15 15:31:27 +0300

    task 4

--- script.js
+++ script.js
@@ -22,14 +22,43 @@
 
 // 3. Створіть функцію, яка випадковим чином виводить одне з трьох повідомлень в консоль кожні 2 секунди.
 
-let massages = ['один', 'два', 'три'];
+// let massages = ['один', 'два', 'три'];
 
-let randomMessage = massages => {
-	setInterval(() => {
-		let randomIndex = Math.floor(Math.random() * massages.length);
-		console.log(massages[randomIndex]);
-	}, 2000);
-};
-randomMessage(massages);
+// let randomMessage = massages => {
+// 	setInterval(() => {
+// 		let randomIndex = Math.floor(Math.random() * massages.length);
+// 		console.log(massages[randomIndex]);
+// 	}, 2000);
+// };
+// randomMessage(massages);
 
 // 4. Всі завдання зі статті: https://uk.javascript.info/settimeout-setinterval
+// 4.1 Напишіть функцію printNumbers(from, to) яка виводить число кожну секунду, починаючи від from і закінчуючи to.
+// Зробіть два варіанти рішення. Використовуючи setInterval. Використовуючи вкладений setTimeout.
+
+// Варіант 1
+
+// let printNumbers = (from, to) => {
+// 	let interval = setInterval(() => {
+// 		console.log(from);
+// 		if (from === to) {
+// 			clearInterval(interval);
+// 		}
+// 		from++;
+// 	}, 1000);
+// };
+// printNumbers(1, 5);
+
+// Варіант 2
+
+// let printNumbers = (from, to) => {
+// 	function time() {
+// 		if (from <= to) {
+// 			console.log(from);
+// 			setTimeout(time, 1000);
+// 		}
+// 		from++;
+// 	}
+// 	setTimeout(time, 1000);
+// };
+// printNumbers(1, 5);
